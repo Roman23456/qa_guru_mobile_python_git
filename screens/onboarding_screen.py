c7c5c7d112db8260c6a065e8907595e64a0c7684
Roman Semenov <r_semenov@tochka.com>
2026-05-17 14:00:13 +0300

    test

--- screens/onboarding_screen.py
+++ screens/onboarding_screen.py
@@ -1,16 +1,21 @@
 import allure
+from appium.webdriver.common.appiumby import AppiumBy
 from selene import be, browser
 
 
 class OnboardingScreen:
     @allure.step('Нажать кнопку "Пропустить" на экране онбординга')
     def skip(self):
-        browser.element('#fragment_onboarding_skip_button').click()
+        browser.element(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")
+        ).click()
         return self
 
     @allure.step("Убедиться, что отображается главный экран с поиском")
     def should_be_visible(self):
-        browser.element('Search Wikipedia').should(be.visible)
+        browser.element(
+            (AppiumBy.XPATH, '//*[@text="Search Wikipedia"]')
+        ).should(be.visible)
         return self
 
 
--- screens/search_screen.py
+++ screens/search_screen.py
@@ -1,31 +1,42 @@
 import allure
+from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
 class SearchScreen:
     @allure.step("Открыть строку поиска")
     def open(self):
-        browser.element('Search Wikipedia').click()
+        browser.element(
+            (AppiumBy.XPATH, '//*[@text="Search Wikipedia"]')
+        ).click()
         return self
 
     @allure.step("Ввести поисковый запрос '{query}'")
     def search(self, query: str):
-        browser.element('#search_src_text').type(query)
+        browser.element(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")
+        ).type(query)
         return self
 
     @allure.step("Убедиться, что список результатов не пустой")
     def should_have_results(self):
-        browser.all('#page_list_item_container').should(have.size_greater_than(0))
+        browser.all(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_container")
+        ).should(have.size_greater_than(0))
         return self
 
     @allure.step("Убедиться, что первый результат содержит '{text}'")
     def first_result_should_have_text(self, text: str):
-        browser.all('#page_list_item_title').first.should(have.text(text))
+        browser.all(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
+        ).first.should(have.text(text))
         return self
 
     @allure.step("Открыть первый результат")
     def open_first_result(self):
-        browser.all('#page_list_item_container').first.click()
+        browser.all(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_container")
+        ).first.click()
         return self
 
 
